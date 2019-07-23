8f688d128d3477e1c2c8c264fe1728c480333a70
vlong638 <vlong638@163.com>
2019-07-24 00:25:59 +0800

    Scanner经典场景

--- commonjavaapis/src/main/java/javautils/ScannerTest.java
+++ commonjavaapis/src/main/java/javautils/ScannerTest.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class ScannerTest {
     public void test() {
-        //基础输入测试
+/*        //基础输入测试
         Scanner sc = new Scanner(System.in);
         System.out.println("输入一个数值");
         int num = sc.nextInt();
@@ -13,6 +13,22 @@ public class ScannerTest {
         //Exception in thread "main" java.util.InputMismatchException
         System.out.println("输入任意字符");
         String str = sc.next();
-        System.out.println("输入的字符是：" + str);
+        System.out.println("输入的字符是：" + str);*/
+
+        //Scanner的适用场景演示
+        System.out.println("适用场景演示");
+        String input = "1 fish 23 fish 4 red fish blue fish";
+        System.out.println("字符串："+input );
+        Scanner s = new Scanner(input).useDelimiter("\\s*fish\\s*");
+        System.out.println("扫描一个数值");
+        System.out.println(s.nextInt());
+        System.out.println("扫描一个数值");
+        System.out.println(s.nextInt());
+        System.out.println("扫描一个字符串");
+        System.out.println(s.next());
+        System.out.println("扫描一个字符串");
+        System.out.println(s.next());
+        s.close();
+
     }
 }
