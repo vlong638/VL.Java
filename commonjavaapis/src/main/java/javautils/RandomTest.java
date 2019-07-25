cfed1d5abb34ca92254613df89c89cb018f0df95
vlong638 <vlong638@163.com>
2019-07-26 00:47:37 +0800

    Random

--- commonjavaapis/src/main/java/javautils/RandomTest.java
+++ commonjavaapis/src/main/java/javautils/RandomTest.java
@@ -2,8 +2,13 @@ package javautils;
 
 import Interface.MyTester;
 
+import java.util.Random;
+
 public class RandomTest implements MyTester {
     public void test() {
-
+        Random r = new Random(100);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(r.nextInt());
+        }
     }
 }
